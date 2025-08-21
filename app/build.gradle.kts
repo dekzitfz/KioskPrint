55709cb99999a19583b535f28a75b043a28b6906
adiandrea <dekz.itfz@gmail.com>
2025-08-21 15:52:23 +0800

    setup epson sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,7 @@ android {
 }
 
 dependencies {
-
+    implementation(fileTree("libs") { include("*.jar") })
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
